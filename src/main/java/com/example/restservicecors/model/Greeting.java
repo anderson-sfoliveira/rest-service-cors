11a0c8317772991eb37be463cd71afe3cca096de
Anderson Oliveira <anderson@sfoliveira@gmail.com>
2020-03-28 20:17:26 -0300

    Created RESTful Web Service

---
+++ src/main/java/com/example/restservicecors/model/Greeting.java
@@ -0,0 +1,27 @@
+package com.example.restservicecors.model;
+
+public class Greeting {
+
+	// Greeting = saudação / cumprimento
+	
+	private final long id;
+	private final String content;
+
+	public Greeting() {
+		this.id = -1;
+		this.content = "";
+	}
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
